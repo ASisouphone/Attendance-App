2b80aafe4bf274658a029d6ecf50c8d832774c3b
Hai <duongh1206@gmail.com>
2019-04-17 02:10:58 -0400

    Added styling for navigation

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -3,7 +3,10 @@ import { Link } from 'react-router-dom';
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
-
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 const Navigation = () => (
   <div>
@@ -16,34 +19,31 @@ const Navigation = () => (
 );
 
 const NavigationAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.LANDING}>Landing</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ACCOUNT}>Account</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.ADMIN}>Admin</Link>
-    </li>
-    <li>
-      <SignOutButton />
-    </li>
-  </ul>
+  <div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="title" color="inherit">GVSU Organization</Typography>
+        <Button color="inherit" component={Link} to="/Landing" style={{marginLeft: 'auto'}} >Landing</Button>
+        <Button color="inherit" component={Link} to="/Home">Home</Button>
+        <Button color="inherit" component={Link} to="/Accoung">Account</Button>
+        <Button color="inherit" component={Link} to="/Admin">Admin</Button>
+        <SignOutButton />
+      </Toolbar>
+    </AppBar>
+  </div>
 );
 
 const NavigationNonAuth = () => (
-  <ul>
-    <li>
-      <Link to={ROUTES.HOME}>Home</Link>
-    </li>
-    <li>
-      <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-    </li>
-  </ul>
+  <div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="title" color="inherit">GVSU Organization</Typography>
+        <Button color="inherit" component={Link} to="/Home" style={{marginLeft: 'auto'}} >Home</Button>
+        <Button color="inherit" component={Link} to="/signin">Sign In</Button>
+      </Toolbar>
+    </AppBar>
+  </div>
 );
+
 
 export default Navigation;
